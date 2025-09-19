8fe42da0f1f0bf680f2ab06851be07a8e6902014
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-19 10:45:39 +0330

    [ADD] my password to env

--- src/seed/data/users.data.ts
+++ src/seed/data/users.data.ts
@@ -2,27 +2,15 @@ import { SeedUserType } from "@/seed/types/seed-user.type";
 
 export const usersData: SeedUserType[] = [
   {
-    username: "BijanProgrammer",
-    email: "bijan@gmail.com",
-    password: "1234",
-    picture: null,
-  },
-  {
     username: "Admin",
     email: "admin@gmail.com",
     password: "1234",
     picture: null,
   },
   {
-    username: "Guest",
-    email: "guest@gmail.com",
-    password: "1234",
-    picture: null,
-  },
-  {
-    username: "Test",
-    email: "test@gmail.com",
-    password: "1234",
+    username: "BijanProgrammer",
+    email: "bijan@gmail.com",
+    password: process.env.BIJAN_PROGRAMMER_PASSWORD ?? "1234",
     picture: null,
   },
 ] as const;
