fc80a6796d88877040cada81746f40baf939cc57
BijanProgrammer <bijaneisapour@gmail.com>
2026-08-28 07:38:29 +0330

    [ADD] ignore list to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import eslint from "@eslint/js";
+import { globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+  globalIgnores(["dist"]),
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
